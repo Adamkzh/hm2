2f8900f467b254ee1022d3a92094d12db938c021
JoshJSZ <joshjiav5@gmail.com>
2019-02-28 10:30:57 -0800

    Sun Status Canvas

---
+++ public/scripts/head.js
@@ -0,0 +1,10 @@
+function onPageLoad() {
+    var sunStatusCanvas = $("#sunStatusCanvas");
+    if (sunStatusCanvas) {
+        drawSunStatusCanvas(sunStatusCanvas);
+    }
+}
+
+function drawSunStatusCanvas(canvas) {
+    console.log("These is sun status canvas!");
+}
